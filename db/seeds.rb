d5e51441bd4c288f8bc96fdf69563f8a03bcc0ff
Kenneth Johnson C <kennojc@gmail.com>
2021-08-25 21:05:55 -0400

    To seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+1000.times do
+    Sale.create!(
+        amount: rand(1990..5490),
+        origin: Faker::Coffee.origin,
+        blend: Faker::Coffee.blend_name,
+        date: Faker::Date.between(from: Date.new('2019-1-1'), to: Date.today)
+    )
+end
